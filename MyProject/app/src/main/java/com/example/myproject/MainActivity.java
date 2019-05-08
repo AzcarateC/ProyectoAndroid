c8f213fceb7d3c6f89e24653be9b24f18a0b83c4
Emanuel <emanuelbastons@gmail.com>
2019-05-08 01:57:13 -0300

    Se agrego comportamiento para evaluar 2 imagenes clickeadas

--- MyProject/app/src/main/java/com/example/myproject/MainActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         for (int fila = 0; fila < 8; fila++) {
             for (int colum = 0; colum < 8; colum++) {
                 int numRandom = (int) (Math.random()*6);
-                Resources res = getResources();
+                Resources res = getResources(); //Por cada elemento de la matriz esta levantando las imagenes, podria hacerse una unica vez antes de iterar
                 ImageView img = new ImageView(this);
                 img.setImageDrawable(res.getDrawable(ArrayImage[numRandom]));
                 img.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         v.setSelected(!v.isSelected());
                         if (v.isSelected()) {
-                            setSingleEvent(g_layout);
+							//Estoy dandole comportamiento onClick al ImageView "img"
+							//Cuando clickeo img corre el siguiente codigo
+                            setSingleEvent(g_layout); 
+							//A todas las imagenes les agrega comportamiento, para que si la clickea se evaluen las 2
                         }
                     }
                 });
@@ -64,27 +67,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setSingleEvent(final GridLayout g_layout) {
-        for (int i = 0; i < g_layout.getChildCount(); i++) {
-            ImageView img = (ImageView) g_layout.getChildAt(i);
-            final int fin = i;
-            img.setOnClickListener(new View.OnClickListener() {
+        for (int i = 0; i < g_layout.getChildCount(); i++) { //Para todas las ImageView
+            ImageView img = (ImageView) g_layout.getChildAt(i); 
+            final int fin = i; //Me guardo su id
+            img.setOnClickListener(new View.OnClickListener() { //A cada ImageView le agrego comportamiento onClick
                 @Override
                 public void onClick(View v) {
-                    setSinlgeEvent2(g_layout, fin);
+					//Cada image view al momento en que se clicke(y despues de haber clickeado alguna imagen antes) corre:
+                    setSinlgeEvent2(g_layout, fin); //Recibe la ID de la imagen clickeada
                 }
             });
         }
     }
 
     private void setSinlgeEvent2(final GridLayout g_layout, final int f) {
-        for (int i = 0; i < g_layout.getChildCount(); i++) {
-            ImageView im2 = (ImageView) g_layout.getChildAt(i);
-            final int fin = i;
+        for (int i = 0; i < g_layout.getChildCount(); i++) { //Para todas las imagenes
+            ImageView im2 = (ImageView) g_layout.getChildAt(i); //Levanto la imagen
+            final int fin = i; //Guardo la id de la imagen
             im2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    
+                    //Agrego comportamiento onClick para la segunda imagen
+					//Muestro en pantalla las id de las dos imagenes!
                     Toast.makeText(MainActivity.this, "Primer elemento" + f + " segundo:" + fin, Toast.LENGTH_SHORT).show();
+					//Devuelvo el comportamiento onclick para seleccionar una imagen
                     setSingleEvent(g_layout);
                     
 
@@ -93,5 +99,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+	
+	//Esta bueno!
 
 }
